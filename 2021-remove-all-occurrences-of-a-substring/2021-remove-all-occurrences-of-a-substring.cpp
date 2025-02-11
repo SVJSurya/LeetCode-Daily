b10c3e08604f26d65df9fee254c86f23d594820f
Surya Dev Singh <139603331+SVJSurya@users.noreply.github.com>
2025-02-11 22:54:30 +0530

    Time: 4 ms (15.12%), Space: 9.3 MB (68.15%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,43 +1,9 @@
 class Solution {
-    vector<int> LPS(const string &s) {
-        int n = s.size();
-        vector<int> l(n,0);
-        int len = 0 ,i =1;
-        while(i<len) {
-            if(s[i] == s[len]) {
-                l[i++] = ++len;
-            }
-            else {
-                if(len != 0) {
-                    len = l[len - 1];
-                }else {
-                    l[i++] = 0;
-                }
-            }
-        }
-        return l;
-    }
 public:
     string removeOccurrences(string s, string part) {
-        vector<int> lps = LPS(s);
-        string res;
-        int i = 0 , j = 0;
-
-        for(char c : s) {
-            res.push_back(c);
-            if(res.size() >= part.size()) {
-                bool m = true;
-                for(int k=0;k<=part.size();k++) {
-                    if(res[res.size()-part.size()+k] != part[k]) {
-                        m = false;
-                        break;
-                    }
-                }
-                if(m) {
-                    res.resize(res.size()-part.size());
-                }
-            }
+        while(s.length()>0 && s.length()>s.find(part)){
+           s.erase(s.find(part),part.length());
         }
-        return res;
+        return s;
     }
 };
