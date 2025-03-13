3fe2a6e801a536d43306d3b89837a87f564f39fc
Surya Dev Singh <139603331+SVJSurya@users.noreply.github.com>
2025-03-13 12:11:52 +0530

    Time: 0 ms (100%), Space: 95.1 MB (63.89%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int ptr1 = 0 , ptr2 = 0;
-    int getmin(vector<int> &n1, vector<int> &n2) {
-        if(ptr1 < n1.size() && ptr2 < n2.size()) {
-            return n1[ptr1] < n2[ptr2] ? n1[ptr1++] : n2[ptr2++];
-        } else if(ptr1 < n1.size()) return n1[ptr1++];
-        else if( ptr2 < n2.size()) return n2[ptr2++];
+    int p1 = 0 , p2 = 0;
+    int getmin(vector<int> &n1,vector<int> &n2) {
+        if(p1 < n1.size() && p2 < n2.size()) {
+            return n1[p1] < n2[p2] ? n1[p1++] : n2[p2++]; 
+        } else if( p1 < n1.size()) return n1[p1++];
+        else if( p2 < n2.size()) return n2[p2++];
         return -1;
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size() , n = nums2.size();
+        int n = nums1.size() , m = nums2.size();
+
         if( (m+n)%2 == 0) {
-            for(int i=0;i<(m+n)/2-1;i++) {
+            for(int i=0;i<(m+n)/2 - 1;i++) {
                 int j = getmin(nums1,nums2);
             }
-            return (double) (getmin(nums1,nums2) + getmin(nums1,nums2)) /2;
+            return (double) (getmin(nums1,nums2) + getmin(nums1,nums2)) / 2;
         } else {
             for(int i=0;i<(m+n)/2;i++) {
                 int j = getmin(nums1,nums2);
