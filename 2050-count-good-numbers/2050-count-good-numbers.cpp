a46af4052aab2891d1b994306963cc89262daa23
Surya Dev Singh <139603331+SVJSurya@users.noreply.github.com>
2025-04-13 22:48:41 +0530

    Time: 0 ms (100%), Space: 8 MB (34.93%) - LeetHub

---
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int MOD=1000000007;
+    void solve(long long a, long long n, long long &ans){
+        if(n==0) return;
+
+        if(n%2==0) {
+            solve(a*a%MOD,n/2,ans);
+        }
+        else {
+            solve(a,n-1,ans=(a*ans)%MOD);
+        }
+    }
+    int countGoodNumbers(long long n) {
+        long long even=n/2 + n%2;
+        long long odd=n/2;
+        long long eveans=1;
+        long long oddans=1;
+        solve(5,even,eveans);
+        solve(4,odd,oddans);
+        return (eveans*oddans%MOD);
+    }
+};
