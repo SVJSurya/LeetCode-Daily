ac13d8cb8f348ac9fbe5da4907706d61f6d4586b
Surya Dev Singh <139603331+SVJSurya@users.noreply.github.com>
2025-04-18 00:23:08 +0530

    Time: 0 ms (100%), Space: 24.5 MB (72.44%) - LeetHub

---
+++ 3221-find-the-peaks/3221-find-the-peaks.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& m) {
+        vector<int> peaks;
+        int n = m.size();
+        for(int i = 1;i<n-1;i++) {
+            if(m[i] > m[i-1] && m[i] > m[i+1]) peaks.push_back(i);
+        }
+        return peaks;
+    }
+};
