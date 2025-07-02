dbda173412a552d9c64f6ea82eff9b5d47cfd6f3
Surya Dev Singh <139603331+SVJSurya@users.noreply.github.com>
2025-07-03 00:21:49 +0530

    Time: 0 ms (100%), Space: 17.1 MB (68.82%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int> (2, 0));
-        
-        for(int i=n-1;i>=0;i--){
-            for(int j =0;j<=1;j++){
-                if(j==0){
-                    dp[i][j] += max(0 + dp[i+1][0], -prices[i] + dp[i+1][1]);
-                }
-                if(j==1){
-                    dp[i][j] += max(0 + dp[i+1][1], prices[i] + dp[i+1][0]);
-                }
+        int maxi = 0;
+        for(int i=1;i<n;i++) {
+            if(prices[i] > prices[i-1]) {
+                int c = prices[i] - prices[i-1];
+                maxi += c;
             }
         }
-        return dp[0][0];
+        return maxi;
     }
 };
