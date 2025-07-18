1c157e08e454e253844d7e6d1c9d7b936dc0908e
Surya Dev Singh <139603331+SVJSurya@users.noreply.github.com>
2025-07-18 15:45:47 +0530

    Time: 133 ms (80.15%), Space: 155.1 MB (39.69%) - LeetHub

--- 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
     long long minimumDifference(vector<int>& nums) {
-        int n = nums.size();
-        int k = n/3;
-        vector<long long> l(n), r(n);
-        priority_queue<int> lH;
-        priority_queue<int, vector<int>, greater<int>> rH;
-        long long ls =0 , rs =0 ,mindiff = LLONG_MAX;
+        int n = nums.size() / 3;
+        int sz = nums.size();
 
-        for(int i=0;i<k;i++) {
-            lH.push(nums[i]);
-            ls += nums[i];
-        }
-        l[k-1] = ls;
+        // Left min heap: get n smallest sum from first i
+        priority_queue<int> maxHeap;
+        vector<long long> left(n * 3, 0);
+        long long leftSum = 0;
 
-        for(int i=k;i<n-k;i++) {
-            if(!lH.empty() && nums[i] < lH.top()) {
-                ls += nums[i] - lH.top();
-                lH.pop();
-                lH.push(nums[i]);
+        for (int i = 0; i < 2 * n; ++i) {
+            maxHeap.push(nums[i]);
+            leftSum += nums[i];
+            if (maxHeap.size() > n) {
+                leftSum -= maxHeap.top();
+                maxHeap.pop();
             }
-            l[i] = ls;
-        }
-
-        for(int i = n-1;i>= n-k;i--) {
-            rH.push(nums[i]);
-            rs += nums[i];
-        }
-        r[n-k] = rs;
-
-        for(int i=n-k-1;i>=k-1;i--) {
-            if(!rH.empty() && nums[i] > rH.top()) {
-                rs += nums[i] - rH.top();
-                rH.pop();
-                rH.push(nums[i]);
+            if (maxHeap.size() == n) {
+                left[i] = leftSum;
             }
-            r[i] = rs;
         }
 
-        for(int i = k-1;i<n-k;i++) {
-            mindiff = min(mindiff, l[i] - r[i+1]);
+        // Right max heap: get n largest sum from last i
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        vector<long long> right(n * 3 + 1, 0);
+        long long rightSum = 0;
+
+        for (int i = sz - 1; i >= n; --i) {
+            minHeap.push(nums[i]);
+            rightSum += nums[i];
+            if (minHeap.size() > n) {
+                rightSum -= minHeap.top();
+                minHeap.pop();
+            }
+            if (minHeap.size() == n) {
+                right[i] = rightSum;
+            }
         }
 
-        return mindiff;
+        long long res = LLONG_MAX;
+        for (int i = n - 1; i < 2 * n; ++i) {
+            res = min(res, left[i] - right[i + 1]);
+        }
+
+        return res;
     }
 };
